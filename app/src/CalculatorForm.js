dea66afaecd9ef9104fa236b7a9f706964f3c645
nkartha2 <nkartha2@gmail.com>
2020-03-23 16:36:18 -0500

    creating form to input number

---
+++ app/src/CalculatorForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class CalculatorForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numberToSubmit: 0,
+      errorMessage: ''
+    }
+  }
+
+  checkValidNumber (inputNumber) {
+    // the number should be natural, above 0, and less than or equal to 100
+    if(inputNumber < 0 || inputNumber > 100 || !Number.isInteger(inputNumber)) {
+      this.setState({errorMessage: 'Not a valid number for calculating difference.'})
+    }
+  }
+
+  onNumberChange = (inputNumber) => {
+    const convertedInputNumber = parseInt(inputNumber);
+    this.setState({numberToSubmit: convertedInputNumber})
+  }
+
+  onSubmitNumber = (e) => {
+    e.preventDefault();
+
+    this.checkValidNumber(this.state.numberToSubmit);
+
+    try {
+
+    } catch(e) {
+
+    } finally {
+      if(!this.state.errorMessage) {
+        this.setState({numberToSubmit: 0})
+      }
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={(e) => {this.onSubmitNumber(e)}}>
+        <label>
+          Enter Number
+          <input
+            value={this.state.numberToSubmit ? this.state.numberToSubmit : ""}
+            onChange={(e) => {this.onNumberChange(e.currentTarget.value)}}
+            type="number"
+          />
+        </label>
+        <input value="Submit" type="submit"/>
+        {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
+      </form>
+    );
+  }
+}
+
+export default CalculatorForm;
