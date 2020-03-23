3fd49673bc790c6f94a8b99a5c0ffdade48bcfde
nkartha2 <nkartha2@gmail.com>
2020-03-23 17:45:44 -0500

    performing calculation and displaying on results

--- app/src/CalculatorForm.js
+++ app/src/CalculatorForm.js
@@ -14,47 +14,70 @@ class CalculatorForm extends React.Component {
   checkValidNumber (inputNumber) {
     // the number should be natural, above 0, and less than or equal to 100
     if(inputNumber < 0 || inputNumber > 100 || !Number.isInteger(inputNumber)) {
-      this.setState({errorMessage: 'Not a valid number for calculating difference.'})
+      this.setState({errorMessage: 'Not a valid number for calculating difference.'});
+      return false
     }
-    return
+    return true
   }
 
   onNumberChange = (inputNumber) => {
+    this.setState({errorMessage: ''});
     const convertedInputNumber = parseInt(inputNumber);
-    this.setState({numberToSubmit: convertedInputNumber})
+    this.setState({numberToSubmit: convertedInputNumber});
   }
 
   onSubmitNumber = (e) => {
     e.preventDefault();
 
-    this.checkValidNumber(this.state.numberToSubmit);
-
-    try {
-      fetch();
-    } catch(e) {
-      console.error(e);
-      this.setState({errorMessage: e.errorMessage})
-    } finally {
-      if(!this.state.errorMessage) {
-        this.setState({numberToSubmit: 0})
+    if(this.checkValidNumber(this.state.numberToSubmit)) {
+      try {
+        fetch(this.state.numberToSubmit).then(
+          response => response.json()
+        ).then(
+          json => {
+            this.setState({
+              submittedValue: json.number,
+              calculatedDifference: json.value,
+              occurences: json.occurrences,
+              dateTime: json.datetime,
+              pastDateTime: json.last_datetime
+            })
+          }
+        )
+      } catch(e) {
+        console.error(e);
+        this.setState({errorMessage: e.errorMessage});
+      } finally {
+        if(!this.state.errorMessage.length) {
+          this.setState({numberToSubmit: 0});
+        }
       }
     }
   }
 
   render() {
     return (
-      <form onSubmit={(e) => {this.onSubmitNumber(e)}}>
-        <label>
-          Enter Number
-          <input
-            value={this.state.numberToSubmit ? this.state.numberToSubmit : ""}
-            onChange={(e) => {this.onNumberChange(e.currentTarget.value)}}
-            type="number"
-          />
-        </label>
-        <input value="Submit" type="submit"/>
-        {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
-      </form>
+      <div>
+        <form onSubmit={(e) => {this.onSubmitNumber(e)}}>
+          <label>
+            Enter Number
+            <input
+              value={this.state.numberToSubmit ? this.state.numberToSubmit : ""}
+              onChange={(e) => {this.onNumberChange(e.currentTarget.value)}}
+              type="number"
+            />
+          </label>
+          <input value="Submit" type="submit"/>
+          {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
+        </form>
+        <ul>
+          <li>Submitted Value: {this.state.submittedValue}</li>
+          <li>Calculated Difference: {this.state.calculatedDifference}</li>
+          <li>Number of Times Requested: {this.state.occurences}</li>
+          <li>Date of Time Requested:{this.state.dateTime}</li>
+          {this.state.pastDateTime && <li>Last Requested: {this.state.pastDateTime}</li>}
+        </ul>
+      </div>
     );
   }
 }
--- app/src/__mock__/fetch.js
+++ app/src/__mock__/fetch.js
@@ -1,11 +1,53 @@
+const submittedNumbers = {};
+
+class CalculationOfDifference {
+  constructor(inputNumber) {
+    this.inputForCalc = inputNumber;
+    submittedNumbers[this.inputForCalc] = {
+      occurrences: 0,
+      difference: this.calculateDifference(),
+      dateTime: new Date().toLocaleString(),
+      pastTime: ''
+    }
+  }
+
+  calculateSquareofSums = () => {
+    let sumToSquare = 0;
+    for(let i=1; i < this.inputForCalc + 1; i++) {
+      sumToSquare += i;
+    }
+    return sumToSquare * sumToSquare;
+  }
+
+  calculateSumOfSquares = () => {
+    let sumOfSquares = 0;
+    for(let i=1; i < this.inputForCalc + 1; i++) {
+      const square = i * i;
+      sumOfSquares += square;
+    }
+    return sumOfSquares;
+  }
+
+  calculateDifference = () => {
+    return this.calculateSquareofSums() - this.calculateSumOfSquares();
+  }
+}
+
+
 export default function(inputNumber) {
+  if(!submittedNumbers.hasOwnProperty(inputNumber)) {
+    new CalculationOfDifference(inputNumber);
+  }
+  submittedNumbers[inputNumber].pastTime = submittedNumbers[inputNumber].dateTime;
+  submittedNumbers[inputNumber].dateTime = new Date().toLocaleString()
+
   return Promise.resolve({
     json: () => Promise.resolve({
-      datetime: '',
-      value: '',
+      datetime: submittedNumbers[inputNumber].dateTime,
+      value: submittedNumbers[inputNumber].difference,
       number: inputNumber,
-      occurrences: '',
-      last_datetime: ''
+      occurrences: submittedNumbers[inputNumber].occurrences += 1,
+      last_datetime: submittedNumbers[inputNumber].pastTime
     })
   });
 }
